3b1191b21ed94da94d51d0445c327f3e36efedef
HuangZhibin9 <105227132+HuangZhibin9@users.noreply.github.com>
2023-12-22 17:56:00 +0800

    DeliveryCounter + Shader Graph

---
+++ KitchenChaos/Assets/Scripts/KitchenCounter/DeliveryCounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        //如果玩家手上有物品
+        if (player.HasKitchenObject())
+        {
+            //如果玩家手上的物品是盘子，把盘子销毁
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                plateKitchenObject.DestroyKitchenObject();
+            }
+        }
+    }
+}
